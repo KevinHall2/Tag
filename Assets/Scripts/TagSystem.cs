e8276a53c739dcc547e1d6f5567ac7fcd601b609
KevinHall2 <khjiitech@gmail.com>
2025-03-24 17:43:53 -0500

    Initial commit

---
+++ Assets/Scripts/TagSystem.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TagSystem : MonoBehaviour
+{
+   
+    [SerializeField]
+    private bool _startTagged = false;
+
+    [SerializeField]
+    private float _tagImmunityDuration = 1.0f;
+
+    private bool _tagged = false;
+    private bool _tagImmune = false;
+
+    public bool Tagged { get { return _tagged; } }
+
+    public bool Tag()
+    {
+        //if already tagged, do nothing
+        if (Tagged)
+        {
+            return false;
+        }
+
+        //if immune to tag, do nothing
+        if (_tagImmune)
+        {
+            return false;
+        }
+        _tagged = true;
+        return true;
+    }
+
+    private void SetTagImmuneFalse()
+    {
+        _tagImmune = false;
+    }
+
+    private void Start()
+    {
+        _tagged = _startTagged;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!Tagged)
+            return;
+
+        if (collision.gameObject.TryGetComponent(out TagSystem tagSystem))
+        {
+            if (tagSystem.Tag())
+            {
+                _tagged = false;
+                _tagImmune = true;
+                Invoke(nameof(SetTagImmuneFalse), _tagImmunityDuration);
+            }
+        }
+    }
+}
